b3d1a0cc6a82769cc289691bf9e212bee5fb6ca3
Lakma Sanjula <64280626+LakmaSanjula-97@users.noreply.github.com>
2023-09-22 18:06:04 +0530

    Updated auth.js

--- Backend/auth/auth.js
+++ Backend/auth/auth.js
@@ -23,60 +23,7 @@ router.post("/register", async (req, res) => {
     }
 }); 
 
-// router.post("/register", async (req, res) => {
-//   try {
-//     const salt = await bcrypt.genSalt(10);
-//     const hashPassword = await bcrypt.hash(req.body.password, salt);
-//     const newUser = new User({
-//       username: req.body.username,
-//       email: req.body.email,
-//       password: hashPassword,
-//     });
-
-//     const user = await newUser.save();
-//     console.log("User registered:", user);
-
-//     // Sending email after successful registration
-//     const transporter = nodemailer.createTransport({
-//       service: "Gmail",
-//       auth: {
-//         user: "@gmail.com", // Replace with your Gmail email
-//         pass: "7", // Replace with your Gmail password
-//       },
-//     });
-
-
-//     const mailOptions = {
-//       from: "@gmail.com",
-//       to: user.email,
-//       subject: "Welcome to Our App",
-//       text: "Thank you for registering with us!",
-//     };
-
-
-//     transporter.sendMail(mailOptions, (error, info) => {
-//       if (error) {
-//         console.log(error);
-//         return res.status(500).json({ error: "Email sending failed." });
-//       } else {
-//         console.log("Email sent: " + info.response);
-//         return res
-//           .status(200)
-//           .json({ message: "Registration successful. Email sent." });
-//       }
-//     });
-
-//     res.status(200).json(user);
-
-
-//   } catch (err) {
-//     res.status(500).json({err});
-//   }
-// });
-
-
-// LOGIN
-
+//LOGIN
 
 router.post("/login", async (req, res) => {
   try {
