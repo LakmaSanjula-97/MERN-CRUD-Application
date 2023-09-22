32a9f986127878d6fdea2da403e236bfe2ef500e
LakmaSanjula-97 <lakmagamage97@gmail.com>
2023-09-22 17:54:18 +0530

    eventpost model

---
+++ Backend/models/EventPost.model.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    photo: {
+      type: String,
+      required: false,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    venue: {
+      type: Array,
+      required: false,
+    },
+    edate: {
+      type: String,
+      required: true,
+    },
+    etime: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Post", PostSchema);
